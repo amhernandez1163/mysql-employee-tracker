e732d8a7725f154d61d28e864df6b2b81985ea83
Amanda Dell <amhernandez1163@gmail.com>
2022-04-30 16:19:06 -0500

    seeds and schema working - began promptMenu functionality and inquirer

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,22 +7,22 @@ VALUES
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-('Data Scientist', '100000', 1),
-('AVP of Analytics', '130000', 1),
-('Junior Developer', '70000', 2),
-('Director of Dev Ops', '180000', 2),
-('Senior Sales Rep' '75000', 3),
-('Director of Sales', '200000', 3),
-('Marketing & Communication Specialist', '60000', 4),
-('AVP of Marketing', '130000', 4);
+('Data Scientist', 100000, 1),
+('AVP of Analytics', 130000, 1),
+('Junior Developer', 70000, 2),
+('Director of Dev Ops', 180000, 2),
+('Senior Sales Rep', 75000, 3),
+('Director of Sales', 200000, 3),
+('Marketing Specialist', 60000, 4),
+('AVP of Marketing', 130000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-('Sarah', 'Sanchez', 1, NULL),
-('Kayla', 'Hudson', 2, 1),
-('Travis', 'Gonzales', 3, NULL),
-('Pat', 'Halentine', 4, 3),
-('Ginger', 'Pak', 5, NULL),
-('Allyson', 'Harlin', 6, 5),
-('Clarissa', 'Jinn', 7, NULL),
-('Sophia', 'Ben', 8, 7);
+('Kayla', 'Hudson', 1, NULL),
+('Sarah', 'Sanchez', 2, 1),
+('Pat', 'Halentine', 3, NULL),
+('Travis', 'Gonzales', 4, 3),
+('Allyson', 'Harlin', 5, NULL),
+('Ginger', 'Pak', 6, 5),
+('Sophia', 'Ben', 7, NULL),
+('Clarissa', 'Jinn', 8, 7);
